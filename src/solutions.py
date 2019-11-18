60d53147abbf477339408a43559b7471abb56aa3
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-11-18 16:06:31 -0600

    Getting all solutions

--- src/solutions.py
+++ src/solutions.py
@@ -1,9 +1,10 @@
 """Queens puzzle solver using backtracking"""
 
+import copy
 
-def ensure_queen(board, row, col):
+
+def ensure_queen(board, row, col, n):
     """Returns True if queen is safe, otherwise returns False"""
-    N = len(board)
 
     # Check this row on left side since there's no need of checking right side
     for i in range(col):
@@ -17,9 +18,33 @@ def ensure_queen(board, row, col):
             return False
 
     # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1),
+    for i, j in zip(range(row, n, 1),
                     range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
+
+
+def solve_puzzle_util(board, col, n):
+    """Generates every board solution"""
+    # If we reached last column, yields that solution
+    if (col == n):
+        yield copy.deepcopy(board)
+    # If we haven't reached last column, we must continue
+    else:
+        for row in range(n):
+            if ensure_queen(board, row, col, n):
+                board[row][col] = 1
+
+                for solution in solve_puzzle_util(board, col + 1, n):
+                    yield solution
+                board[row][col] = 0
+
+
+def solve_puzzle(n):
+    """Given an n, returns all possible solutions for a n size board"""
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    for solution in solve_puzzle_util(board, 0, n):
+        yield solution
--- src/tests/test_solvers.py
+++ src/tests/test_solvers.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from ..solutions import ensure_queen
+from ..solutions import ensure_queen, solve_puzzle
 
 
 class SolverTestCase(unittest.TestCase):
@@ -16,9 +16,8 @@ class SolverTestCase(unittest.TestCase):
             [0, 0, 0, 0],
             [0, 1, 0, 0],
         ]
-        self.assertFalse(ensure_queen(board, 1, 3))
+        self.assertFalse(ensure_queen(board, 1, 3, 4))
 
-    # @unittest.skip('Diagonals not implemented')
     def test_ensure_queen_wrong_diagonal(self):
         """Make sure that ensure_queen function works for diagonals"""
         board = [
@@ -27,9 +26,8 @@ class SolverTestCase(unittest.TestCase):
             [0, 0, 0, 0],
             [0, 1, 0, 0],
         ]
-        self.assertFalse(ensure_queen(board, 2, 3))
+        self.assertFalse(ensure_queen(board, 2, 3, 4))
 
-    # @unittest.skip('Not accurate')
     def test_ensure_queen_right_board(self):
         """Make sure that ensure_queen function works for right boards"""
         board = [
@@ -38,7 +36,39 @@ class SolverTestCase(unittest.TestCase):
             [0, 0, 0, 0],
             [0, 1, 0, 0],
         ]
-        self.assertTrue(ensure_queen(board, 2, 3))
+        self.assertTrue(ensure_queen(board, 2, 3, 4))
+
+    def test_solve_puzzle_solution_number(self):
+        """Ensures we are getting all solutions"""
+        n_1 = len([board for board in solve_puzzle(1)])
+        n_2 = len([board for board in solve_puzzle(2)])
+        n_4 = len([board for board in solve_puzzle(4)])
+        n_5 = len([board for board in solve_puzzle(5)])
+
+        self.assertEqual(n_1, 1)
+        self.assertEqual(n_2, 0)
+        self.assertEqual(n_4, 2)
+        self.assertEqual(n_5, 10)
+
+    def test_solve_puzzle_solutions(self):
+        """Ensures given solutions are correct"""
+        solutions = [
+            [
+                [0, 0, 1, 0],
+                [1, 0, 0, 0],
+                [0, 0, 0, 1],
+                [0, 1, 0, 0],
+            ],
+            [
+                [0, 1, 0, 0],
+                [0, 0, 0, 1],
+                [1, 0, 0, 0],
+                [0, 0, 1, 0],
+            ]
+        ]
+
+        for solution in solve_puzzle(4):
+            self.assertIn(solution, solutions)
 
 
 if __name__ == "__main__":
